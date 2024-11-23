43d273e08698a8352a7c48f1a78a46ca9c9d7053
enkalamessi <nati.israel2019@gmail.com>
2024-11-23 16:30:03 +0200

    ok

--- Website/auth.py
+++ Website/auth.py
@@ -1,10 +1,10 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 auth = Blueprint('auth', __name__)
 
 @auth.route("/login")
 def login():
-    return "<button>Login</button>"
+    return render_template('login.html')
 
 
 @auth.route("/logout")
