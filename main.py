0da0bdca3549b49b476f6342b8f13c3d857a16cc
enkalamessi <nati.israel2019@gmail.com>
2024-11-23 16:36:24 +0200

    ok

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
+import os
 from Website import create_app
 
 
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    # Get the port from the environment, default to 5000 if not set
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port,debug=True)
